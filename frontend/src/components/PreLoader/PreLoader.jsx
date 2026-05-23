c03ccc9fda198734af22a2d90c2a6b8ebda6145f
Raj Nandani Maurya <147945705+Nandani-Maurya@users.noreply.github.com>
2026-05-23 09:36:18 +0530

    commit

--- frontend/src/components/PreLoader/PreLoader.jsx
+++ frontend/src/components/PreLoader/PreLoader.jsx
@@ -1,9 +1,18 @@
-import './PreLoader.css';
+import headerBackground from "../../assets/header_backgroung.png";
+import verticalLogo from "../../assets/vertical_logo.png";
+import "./PreLoader.css";
 
 const PreLoader = () => {
   return (
-    <div className="preloader-container">
-      <div className="loader"></div>
+    <div
+      className="preloader-container"
+      style={{ "--preloader-bg": `url(${headerBackground})` }}
+    >
+      <div className="preloader-mark" aria-label="Loading Banarasi Kala">
+        <span className="preloader-ring" aria-hidden="true" />
+        <img src={verticalLogo} alt="Banarasi Kala" />
+        <span className="preloader-progress" aria-hidden="true" />
+      </div>
     </div>
   );
 };
